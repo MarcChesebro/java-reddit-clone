70c16d6e537374052a9920974c49301a6287ec87
Marc Chesebro <Marc.chesebro@gmail.com>
2018-12-02 23:56:40 -0500

    started server structure

--- src/Reddit/PageList.java
+++ src/Reddit/PageList.java
@@ -8,7 +8,18 @@ public class PageList {
     	Page newPage = new Page(title);
     	pages.add(newPage);
     }
+
     public ArrayList<Page> getPages(){
     	return pages;
+    }
+
+    public Page getPage(String pageTitle){
+
+        for(int i = 0; i < this.pages.size(); i++){
+            if (this.pages.get(i).getTitle().equals(pageTitle)){
+                return this.pages.get(i);
+            }
+        }
+        return null;
     }
 }
--- src/Reddit/ServerThread.java
+++ src/Reddit/ServerThread.java
@@ -10,9 +10,23 @@ public class ServerThread implements Runnable{
     private static int threadCount;
     private int user;
     private Socket connection;
+    private PageList pageList;
 
     public ServerThread(Socket socket) throws Exception {
         this.connection = socket;
+        this.pageList = new PageList();
+    }
+
+    public void addPage(String Title){
+        this.pageList.addPage(Title);
+    }
+
+    public void addPost(String pageTitle, String postText, String image){
+        //TODO this is assuming the image has been downloaded already.
+        Page page = this.pageList.getPage(pageTitle);
+        if (page != null){
+            page.addPost(postText, image);
+        }
     }
 
     // this runs on start after the thread has been setup
